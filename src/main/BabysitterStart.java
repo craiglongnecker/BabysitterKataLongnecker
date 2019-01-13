54447665665c3068ecba1803b3c1fbe2094a19c7
Craig Longnecker <craig2020@q.com>
2019-01-13 14:56:55 -0600

    Initial commit

---
+++ src/main/BabysitterStart.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BabysitterStart {
+
+	public static void main(String[] args) throws IOException { // Begin main.
+		Scanner scanner = new Scanner(System.in);
+
+	} // Close main.
+
+	public static String BabysitterName(String name) {
+		return "Julie";
+	}
+
+}
---
+++ src/tests/BabysitterStartTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.BabysitterStart;
+
+public class BabysitterStartTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void isBabysittersNameJulie() {
+		assertEquals("Julie", BabysitterStart.BabysitterName("Julie"));
+	}
+}
